875c1bde2f24ff81d4faea73be7b3a1313adcb11
xieguanping12 <diudiu1148@gmail.com>
2018-04-07 16:34:17 +0800

    更新用户

--- manage.js
+++ manage.js
@@ -1,10 +1,13 @@
 //引入模块包
 let express = require('express');
 let path = require('path');
+let bodyParser = require('body-parser');//解析post请求体
 
 app = express(); //创建一个express应用
 
 app.use(express.static('public'));//前端资源文件根目录
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');//设置模板引擎ejs
 
--- routers/admin.js
+++ routers/admin.js
@@ -19,9 +19,41 @@ router.get('/user/list', function (req, res) {
 
 router.get('/user', function (req, res) {
     Users.findById(req.query._id, function (err, data) {
-        // console.log(req.query._id,data);
         if (data) {
             res.render('admin/user/detail', {title: "用户详情", user: data})
+        } else {
+            res.json({"error": true, "message": "用户不存在"});
+        }
+    });
+});
+
+router.get('/user/update', function (req, res) {
+    _id = req.query._id;
+    Users.findById(_id, function (err, data) {
+        if (data) {
+            res.render('admin/user/update', {title: "修改用户", user: data});
+        } else {
+            res.json({"error": true, "message": "用户不存在"});
+        }
+    });
+});
+
+router.post('/user/update', function (req, res) {
+    _id = req.query._id;
+    Users.findById(_id, function (err, data) {
+        if (data) {
+            updateData = {
+                name: req.body.name,
+                passwd: req.body.passwd,
+                email: req.body.email
+            };
+            Users.where({_id: _id}).update({$set: updateData}, function (err, data) {
+                if (err) {
+                    res.json({"error": true, "message": err.message});
+                } else {
+                    res.redirect('/admin/user/list');
+                }
+            });
         } else {
             res.json({"error": true, "message": "用户不存在"});
         }
